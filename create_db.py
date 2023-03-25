e2f0e306042dd3cb88a7374c27f35b206101c410
Некит программист < @DESKTOP-0N9MUS7>
2023-03-25 15:22:10 +0700

    upd create db script

--- create_db.py
+++ create_db.py
@@ -1,15 +1,23 @@
-from sqlalchemy import text
-from energy_gym_server.models.database import Base, engine, User
-from energy_gym_server.models import UserRoles
+from energy_gym_mainserver.orm import Base
+from energy_gym_mainserver.orm import engine
+from energy_gym_mainserver.orm import User
+from energy_gym_mainserver.models import UserRole
+from energy_gym_mainserver.services import UsersService
 
 
 def start_base():
     try:
         print('Создание таблиц в БД...')
         Base.metadata.create_all(engine)
-        with engine.begin() as conn:
-            conn.execute(
-                text(f"INSERT INTO {User.__tablename__} VALUES (-1, 'ADMIN', '-', 'hexReGON14', '{UserRoles.ADMIN.name}')")
+        with UsersService() as service:
+            service.create(
+                User(
+                    student_card = -1,
+                    name         = 'ADMIN',
+                    group        = '-',
+                    password     = 'hexReGON14',
+                    role         = UserRole.ADMIN.name,
+                )
             )
     except Exception as e:
         print(f'Ошибка создания таблиц: {e}')
