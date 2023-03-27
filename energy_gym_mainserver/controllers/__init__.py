a9ca0081adef2b1798acefee32b39b5ce46a7987
Некит программист < @DESKTOP-0N9MUS7>
2023-03-27 19:54:49 +0700

    factory for controllers

--- energy_gym_mainserver/controllers/__init__.py
+++ energy_gym_mainserver/controllers/__init__.py
@@ -2,3 +2,4 @@ from .available_time import AvailableTimeDBController
 from .entry import EntryDBController
 from .user import UserDBController
 from .converter import DTOConverter
+from .factory import ControllerFactory
---
+++ energy_gym_mainserver/controllers/factory.py
@@ -0,0 +1,34 @@
+from .available_time import AvailableTimeDBController
+from .entry import EntryDBController
+from .user import UserDBController
+from .converter import DTOConverter
+from ..services import AvailableTimeDBService
+from ..services import EntryDBService
+from ..services import UserDBService
+
+
+class ControllerFactory:
+
+    @classmethod
+    def avtime(cls) -> AvailableTimeDBController:
+        return AvailableTimeDBController(
+            AvailableTimeDBService,
+            EntryDBService,
+            DTOConverter()
+        )
+    
+    @classmethod
+    def entry(cls) -> EntryDBController:
+        return EntryDBController(
+            EntryDBService,
+            AvailableTimeDBService,
+            UserDBService,
+            DTOConverter()
+        )
+    
+    @classmethod
+    def user(cls) -> UserDBController:
+        return UserDBController(
+            UserDBService,
+            DTOConverter()
+        )
