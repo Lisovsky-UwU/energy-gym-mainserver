fe9d6856ce9770508c4360b9d57b1031cc7ad3c0
Некит программист < @DESKTOP-0N9MUS7>
2023-04-02 16:26:08 +0700

    add ads and tokens tables

---
+++ energy_gym_mainserver/orm/ads.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Boolean
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from . import Base
+
+
+class Ads(Base):
+    
+    __tablename__ = 'ads'
+
+    id              = Column(Integer, primary_key=True, autoincrement=True)
+    create_time     = Column(DateTime, nullable=False, default=datetime.now)
+    body            = Column(String, nullable=False)
+    user            = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    deleted         = Column(Boolean, default=False)
+
+    users           = relationship('User', back_populates='ads')
--- energy_gym_mainserver/orm/user.py
+++ energy_gym_mainserver/orm/user.py
@@ -12,11 +12,13 @@ class User(Base):
     __tablename__ = 'users'
 
     id            = Column(Integer, primary_key=True, autoincrement=True)
-    student_card  = Column(Integer, nullable=False, unique=True)
+    student_card  = Column(Integer, nullable=False, unique=True, index=True)
     name          = Column(String(70), nullable=False)
     group         = Column(String(20), nullable=False)
     password      = Column(String(60), nullable=False)
-    role          = Column(String(15), nullable=False)
+    role          = Column(String(15), nullable=False, index=True)
     deleted       = Column(Boolean, default=False)
 
+    ads           = relationship('Ads', uselist=False, back_populates='users')
+    tokens        = relationship('Token', uselist=False, back_populates='users')
     entries       = relationship('Entry', uselist=False, back_populates='users')
