eba4d863d46e3109146bc16e145ba54f60a5267a
Некит программист < @DESKTOP-0N9MUS7>
2023-04-05 17:43:29 +0700

    entry api

--- energy_gym_mainserver/blueprints/api/entry.py
+++ energy_gym_mainserver/blueprints/api/entry.py
@@ -3,7 +3,6 @@ from flask import request
 
 from ...controllers import ControllerFactory
 from ...models import dto
-from ...exceptions import InvalidRequestException
 
 
 entry_bl = Blueprint('entry', 'entry')
@@ -11,43 +10,59 @@ entry_bl = Blueprint('entry', 'entry')
 
 @entry_bl.post('/create')
 def create_entry():
-    return {'result': 'in develop...'}
+    return ControllerFactory.entry().create_by_user(
+        int(request.headers.get('user-id')),
+        request.get_json()
+    ).dict()
 
 
 @entry_bl.post('/create-any')
 def create_any_entry():
     return ControllerFactory.entry().create_for_list(
-        dto.EntryList.parse_obj(request.json)
+        dto.EntryAddList(
+            data = list(
+                dto.EntryAddRequest.parse_obj(entry)
+                for entry in request.get_json()
+            )
+        )
     ).dict()
 
 
 @entry_bl.get('/get')
 def get_entries():
-    return {'result': 'in develop...'}
+    return ControllerFactory.entry().get_for_user(
+        int(request.headers.get('user-id'))
+    ).dict()
 
 
 @entry_bl.get('/get-any')
 def get_any_entries():
+    controller = ControllerFactory.entry()
     try:
         data = request.get_json()
     except:
-        raise InvalidRequestException('Тело запроса должно быть в формате JSON')
-    
-    controller = ControllerFactory.entry()
+        return controller.get_all().dict()
 
     if 'user' in data:
         return controller.get_for_user(data['user']).dict()
     elif 'avtime' in data:
         return controller.get_for_avtime(data['avtime']).dict()
     else:
-        return ControllerFactory.entry().get_all().dict()
+        return controller.get_all().dict()
 
 
 @entry_bl.delete('/delete')
 def delete_entries():
-    return {'result': 'in develop...'}
+    return ControllerFactory.entry().delete_for_id_list(
+        request.get_json(),
+        int(request.headers.get('user-id'))
+    ).dict()
 
 
 @entry_bl.delete('/delete-any')
 def delete_any_entries():
-    return {'result': 'in develop...'}
+    return ControllerFactory.entry().delete_for_id_list(
+        request.get_json(),
+        int(request.headers.get('user-id')),
+        True
+    ).dict()
