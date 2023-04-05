ec2f932de071d7cdd32c481b31f82c51e67cc43f
Некит программист < @DESKTOP-0N9MUS7>
2023-04-05 16:39:21 +0700

    add cheker in avtime

--- energy_gym_mainserver/controllers/available_time.py
+++ energy_gym_mainserver/controllers/available_time.py
@@ -3,6 +3,7 @@ from typing import Iterable
 from typing import List
 from typing import Type
 
+from ..exceptions import LogicError
 from ..services import AvailableTimeDBService
 from ..models import dto
 from ..orm import AvailableTime
@@ -37,9 +38,29 @@ class AvailableTimeDBController:
             return self.__to_list_model__(av_times)
 
 
+    def from_orm_to_model(self, _from: AvailableTime) -> dto.AvailableTimeModel:
+        result = dto.AvailableTimeModel.from_orm(_from)
+        result.free_seats = _from.number_of_persons - len(_from.not_deleted_entries)
+        return result
+
+
     def all_id_list_in_db(self, id_list: List[int]) -> bool:
         with self.av_service_type() as service:
             return all( service.get_by_id(avtime_id) is not None for avtime_id in id_list )
+
+
+    def check_create_for_id_list(self, id_list: List[int]):
+        '''Поднимет ошибку LogicError в случае несоответствия'''
+        with self.av_service_type() as service:
+            avtimes = list( service.get_by_id(avtime_id) for avtime_id in id_list )
+
+            for avtime in avtimes:
+                if avtime is None:
+                    raise LogicError('Не найдено одно из доступных времен для записи')
+                if avtime.number_of_persons - len(avtime.not_deleted_entries) <= 0:
+                    raise LogicError('Отсутствуют места для записи на одно из времен')
+                if len([ _avt for _avt in avtimes if _avt.weekday == avtime.weekday ]) > 1:
+                    raise LogicError('Максимальное число записей на один день - 1')
 
 
     def __to_list_model__(self, data: Iterable[AvailableTime]) -> dto.AvailableTimeList:
@@ -48,15 +69,6 @@ class AvailableTimeDBController:
 
     def __to_models__(self, data: Iterable[AvailableTime]) -> List[dto.AvailableTimeModel]:
         return [
-            self.__from_orm_to_model__(
-                _from      = av_time,
-                free_seats = av_time.number_of_persons - len(av_time.entries)
-            )
+            self.from_orm_to_model(av_time)
             for av_time in data
         ]
-
-    
-    def __from_orm_to_model__(self, _from: AvailableTime, free_seats: int) -> dto.AvailableTimeModel:
-        result = dto.AvailableTimeModel.from_orm(_from)
-        result.free_seats = free_seats
-        return result
