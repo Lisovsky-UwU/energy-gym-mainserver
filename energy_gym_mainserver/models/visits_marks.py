51c8310256a9273720714247b0ac690ce7afc5d9
Некит программист < @DESKTOP-0N9MUS7>
2023-04-08 22:06:02 +0700

    create visit marks

---
+++ energy_gym_mainserver/models/visits_marks.py
@@ -0,0 +1,7 @@
+class VisitMark:
+    
+    PASS       = 0 # Пропуск
+    ATTENDED   = 1 # Присутствовал
+    VALID_PASS = 2 # Пропуск по уважительной
+    CANCELLED  = 3 # Занятие отменено
+    BLOCKED    = 4 # Пользователь заблокирован
